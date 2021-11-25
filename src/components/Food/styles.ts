94180a6cf84570a0bac02f49f729c7ee6bbc46c8
lucas-montenegro <lucasmaassuncao@gmail.com>
2021-11-24 23:27:51 -0300

    Initial transition to typescript

--- src/components/Food/styles.ts
+++ src/components/Food/styles.ts
@@ -1,6 +1,10 @@
 import styled, { css } from 'styled-components';
 
-export const Container = styled.div`
+interface FoodHeaderProps {
+  available: boolean,
+}
+
+export const Container = styled.div<FoodHeaderProps>`
   background: #f0f0f5;
   border-radius: 8px;
 
